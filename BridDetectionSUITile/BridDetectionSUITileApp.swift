1a857f4d986a4d681891fd5cd12175e33c5f5d9b
Dimitreee <Dmitrii.priimuk@gmail.com>
2021-08-07 12:26:57 +0200

    Initial Commit

---
+++ BridDetectionSUITile/BridDetectionSUITileApp.swift
@@ -0,0 +1,17 @@
+//
+//  BridDetectionSUITileApp.swift
+//  BridDetectionSUITile
+//
+//  Created by Dmitrii on 07.08.2021.
+//
+
+import SwiftUI
+
+@main
+struct BridDetectionSUITileApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
